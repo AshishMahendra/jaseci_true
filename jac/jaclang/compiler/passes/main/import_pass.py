1800632a790b161acae563d06e73c647a252a3a1
Ashish Mahendra <ashishmahendra@live.com>
2025-01-14 16:24:14 +0000

    test_again

--- jac/jaclang/compiler/passes/main/import_pass.py
+++ jac/jaclang/compiler/passes/main/import_pass.py
@@ -176,7 +176,7 @@ class JacImportPass(Pass):
         from jaclang.compiler.passes.main import SubNodeTabPass
 
         if not os.path.exists(target):
-            print("Hello error from import pass")
+            print("Hello error from import pass testing again")
             self.error(f"Could not find module {target}")
             return None
         if target in self.import_table:
